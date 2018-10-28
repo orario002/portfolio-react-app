56dce78775c8a2381ff0d70f034c5c3a191fe7f6
sandyc <sandyc@ensemble.com>
2018-10-28 18:39:30 +0000

    Add pages place holders.

--- src/Components/About.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About = () => (
-  <div>About here</div>
-  );
-
-  export default About;
---
+++ src/Pages/Blog.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Blog = () => (
+  <div>Blog here</div>
+  );
+
+  export default Blog;
