657affc10d730920c986c1d1089670adca353573
sandyc <sandyc@ensemble.com>
2019-01-16 00:31:58 +0000

    Add Type Form for contact page.

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React from 'react';
+import * as typeformEmbed from '@typeform/embed'
 
-const Contact = () => (
-  <div>Contact here</div>
-  );
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.el = null;
+  }
+  componentDidMount() {
+    if (this.el) {
+      typeformEmbed.makeWidget(this.el, "https://sandy413.typeform.com/to/zn9h04", {
+        hideFooter: true,
+        hideHeaders: true,
+        opacity: 0
+      });
+    }
+  }
+  render() {
+    return (
+      <div ref={(el) => this.el = el} style={{width: '100%', height: '300px'}} />
+    )
+  }
+}
 
-  export default Contact;
+export default Contact;
