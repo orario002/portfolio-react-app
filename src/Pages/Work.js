47f31932e814bb39493fbffe61d30aadfcac51fe
sandyc <orario002@hotmail.com>
2019-01-20 21:53:18 +0000

    Use map and json for repeated items.

--- src/Pages/Work.js
+++ src/Pages/Work.js
@@ -1,58 +1,49 @@
-import React, {Component} from "react";
-import {Media, Row} from "reactstrap";
+import React from "react";
+import {Col} from "reactstrap";
 import Sandy from "../Assets/mockup/mockup-sandychiu-co-uk.png";
 import Olivet from "../Assets/mockup/mockup-olivet.png";
 import Wedding from "../Assets/mockup/mockup-sandyandstephen.png";
 import Solo from "../Assets/mockup/mockup-solomeworld.png";
 
-class Work extends Component {
+const JSON = {
+    Sandy: {
+        name: 'sandys',
+        portfolioImage: `${Sandy}`,
+        link: 'http://sandychiu.co.uk'
+    },
+    Olivet: {
+        name:'olivet',
+        portfolioImage: `${Olivet}`,
+        link: 'http://olivet.me',
+    },
+    Wedding: {
+        name: 'sandyandstephen',
+        portfolioImage: `${Wedding}`,
+        link: 'http://sandyandstephen.com'
+    },
+    Solo: {
+        name: 'solomeworld',
+        portfolioImage: `${Solo}`,
+        link: 'http://cchiu02.wdd1516.bbkweb.org/wdd4-web-project/',
+    }
+};
+
+class Work extends React.Component {
     render() {
         return (
-            <div>
-                <Media list>
-                    <Row>
-                        <Media tag="li">
-                            <Media body href="http://olivet.me">
-                                <Media heading>
-                                    Olivet.me
-                                </Media>
-                                <Media object src={Olivet} alt="Olivet image"/>
-                            </Media>
-                        </Media>
-
-                        <Media tag="li">
-                            <Media body href="#">
-                                <Media heading>
-                                    Sandychiu.co.uk
-                                </Media>
-                                <Media object src={Sandy} alt="Sandychiu.co.uk image"/>
-                            </Media>
-                        </Media>
-                    </Row>
-
-                    <Row>
-                        <Media tag="li">
-                            <Media body href="http://sandyandstephen.com">
-                                <Media heading>
-                                    Sandy and stephen
-                                </Media>
-                                <Media object src={Wedding} alt="Sandy and stephen image"/>
-                            </Media>
-                        </Media>
-
-                        <Media tag="li">
-                            <Media body href="http://cchiu02.wdd1516.bbkweb.org/wdd4-web-project/">
-                                <Media heading>
-                                    Solo me world
-                                </Media>
-                                <Media object src={Solo} alt="Solo me world image"/>
-                            </Media>
-                        </Media>
-                    </Row>
-                </Media>
+            <div className="row">
+                {Object.keys(JSON).map(key => (
+                    <Col sm="12" md="6">
+                        <p>{JSON[key].link}</p>
+                        <div>
+                        <img src={JSON[key].portfolioImage} alt={JSON[key].name} key={key} />
+                        </div>
+                    </Col>
+                ))}
             </div>
         );
     }
 }
+
 
 export default Work;
