87e21a0e60961e85447f0b62b2f7207c625e567b
sandyc <orario002@hotmail.com>
2019-01-24 03:06:26 +0000

    editing about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,39 +4,41 @@ import BarChart from "./About/BarChart";
 
 const JSON = {
     am: {
-        title: 'I am',
+        title: 'I work as',
         text: 'Front-end developer and UI/UX designer',
     },
     like: {
         title: 'I enjoy',
-        text: 'UI design, UX design, Graphic design, ',
+        text: 'Design and make things, Traveling',
     },
-    want: {
-        title: 'I want to be',
+    career: {
+        title: 'Career path',
         text: 'Interaction designer, Product designer'
     },
     interest: {
         title: 'I am interested in',
-        text: 'Design, Technology, Art, Photography'
+        text: 'Design, Technology, Art, Entrepreneurship'
     },
     animal: {
         title: 'What would I be if I were an animal',
-        text: 'Unicorn, Koala, Sloth'
+        text: 'Unicorn + Koala + Sloth'
+    },
+    goal: {
+        title: 'What motivates me',
+        text: 'Curiosity'
     }
-}
+};
 
 class About extends Component {
   render() {
     return (
         <div>
-            <div className="">
             <BarChart />
-            </div>
           <div className="row">
               {Object.keys(JSON).map(key => (
-                  <Col xs="12" sm="6" md="4">
-                      <h3>{JSON[key].title}</h3>
-                          <p>{JSON[key].text}</p>
+                  <Col xs="12" sm="6" md="4" className={"p-4"}>
+                      <h3 className="title">{JSON[key].title}</h3>
+                          <p className="description">{JSON[key].text}</p>
                   </Col>
               ))}
           </div>
