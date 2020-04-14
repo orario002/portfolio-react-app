cbe02feec9bee21f61280558e6072a11a70b2bfa
sandyc <orario002@hotmail.com>
2020-04-14 13:05:06 +0100

    Typo and correct image type.

--- src/Data/SiteBuilderData.js
+++ src/Data/SiteBuilderData.js
@@ -11,14 +11,14 @@ import Screens from "../Assets/portfolio/portfolio_sitebuilder_screens.png";
 export const SiteBuilderData = {
     banner: {
         name: 'Hero Banner',
-        title: 'A microsite builder prototype offering B2B solution with SiteBuilder Experience Manager, to create simple webpages effortlessly.',
-        intro: 'Designed for our partner to demonstrate how businesses can utilise SiteBuilder Experience Manager’s smart technology to quickly build microsites. Without technical knowledge, anyone from organisation can simply fill out a form to generate basic webpages using default templates.',
+        title: 'A microsite builder prototype offering B2B solution with Adobe Experience Manager, to create simple webpages effortlessly.',
+        intro: 'Designed for our partner to demonstrate how businesses can utilise Adobe Experience Manager’s smart technology to quickly build microsites. Without technical knowledge, anyone from organisation can simply fill out a form to generate basic webpages using default templates.',
         img: `${Banner}`
     },
     summary: {
         overview: 'Client work with request to design a prototype for businesses, to fill a form that generates basic web pages without technical knowledge. With the abilities to implement with their own customised theme and brand.',
         time: 'Mar 4 to Mar 22, 2019 (3 weeks)',
-        tools: ['SiteBuilder XD', 'Zeplin'],
+        tools: ['Adobe XD', 'Zeplin'],
         roles: ['UX Designer', 'Visual Designer'],
         teams: ['Software Developer', 'Project Manager']
     },
@@ -71,10 +71,10 @@ export const SiteBuilderData = {
     },
     outcome: {
         title: 'Successfully tell product stories through prototyping.',
-        description: 'We were asked to make prototype within SiteBuilder Experience Manager platform, developing using out-of-box component. As a designer, I have to be aware of this constraint and make sure the design fit in the category. This has helped reduce customisation and limit the time our developer spends to create this prototype.',
+        description: 'We were asked to make prototype within Adobe Experience Manager platform, developing using out-of-box component. As a designer, I have to be aware of this constraint and make sure the design fit in the category. This has helped reduce customisation and limit the time our developer spends to create this prototype.',
         deliveries: ['High fidelity wireframes of microsite builder were developed into Interactive prototype.\n',
-            'Because the designs were based on out-of-box components in SiteBuilder Experience Manager, the developers can build the components more efficiently.',
-            'Exported designs from SiteBuilder XD to Zeplin for front-end developers to view specifications.'],
+            'Because the designs were based on out-of-box components in Adobe Experience Manager, the developers can build the components more efficiently.',
+            'Exported designs from Adobe XD to Zeplin for front-end developers to view specifications.'],
         lesson: 'Before we can deliver clear messages through the prototype, we needed to speak to the stakeholders and understand the context of the project. It helps us to understand what matters to them. As a prototype we wanted to campaign product value whilst tell a good story to help potential client imagine using it.'
     }
 };
